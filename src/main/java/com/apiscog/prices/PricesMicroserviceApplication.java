0f5ae693b9bb2abce2351f7a31734637ecc0566a
DESKTOP-25D2AVM\eddya <andres.piscog@gmail.com>
2026-08-19 18:44:59 +0200

    Initial repository of code

---
+++ src/main/java/com/apiscog/prices/PricesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.apiscog.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiscog/prices/PricesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiscog.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
